020c9a6b8d965bf7aa86bafe71eda8a6e9bf58cf
Bergmah <52858177+Bergmah@users.noreply.github.com>
2020-07-07 14:40:42 +0300

    Getting messages in a required chat

--- src/main/java/andrew/projects/reacon/repos/MessageRepo.java
+++ src/main/java/andrew/projects/reacon/repos/MessageRepo.java
@@ -1,9 +1,17 @@
 package andrew.projects.reacon.repos;
 
 import andrew.projects.reacon.entities.Message;
+import andrew.projects.reacon.entities.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import javax.persistence.criteria.CriteriaBuilder;
 
 public interface MessageRepo extends CrudRepository<Message, Integer> {
+
+    @Query("select m from Message m ,Conversation c " +
+            "where m.idConversation=:currentChatId " +
+            "group by m.idMessage")
+    Iterable<User> allMessagesInChat(@Param("currentChatId") Integer id);//todo test
 }
